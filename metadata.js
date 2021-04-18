d7a798149f252fb138f886b5003ba0c41b1c9c8c
luizfernandosg <38767084+luizfernandosg@users.noreply.github.com>
2021-04-18 14:40:11 -0300

    Create metadata.js

---
+++ metadata.js
@@ -0,0 +1,7 @@
+{
+  "description": "This is only a test. I repeat: this is only a test.", 
+  "external_url": "https://www.youtube.com/watch?v=rb4-oOKIZQ8", 
+  "image": "./bluebird.jpeg", 
+  "name": "This is only a test.",
+  "attributes": [ ... ], 
+}
